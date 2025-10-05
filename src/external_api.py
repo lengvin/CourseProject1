b2eb04bb8a33234a8756c2c58c146f4a0dbc5d3d
lengvin <zve565ro@gmail.com>
2025-10-05 12:50:20 +0700

    add function currency_conversion_in_rub

---
+++ src/external_api.py
@@ -0,0 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+headers = {"apikey": f"{api_key}"}
+
+
+def currency_conversion_in_rub(operation):
+    """
+    Конвертация валюты в рубли
+    """
+    currency = operation['operation_currency']
+    amount = operation['operation_amount']
+
+    if currency == "RUB":
+        return amount
+    else:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+
+        response = requests.get(url, headers=headers)
+        response_data = response.json()
+        result = response_data["result"]
+
+        return result
