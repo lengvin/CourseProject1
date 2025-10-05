2918b74983b5bf60e2fe3e76c3c9f7543d0ea892
lengvin <zve565ro@gmail.com>
2025-10-05 22:56:26 +0700

    add function get_currency_rate

--- src/external_api.py
+++ src/external_api.py
@@ -26,3 +26,19 @@ def currency_conversion_in_rub(operation):
         result = response_data["result"]
 
         return result
+
+
+def get_currency_rate(user_settings):
+    """
+    Вывод заданных курсов валют
+    """
+    result = []
+    for currency in user_settings:
+        url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}"
+        response = requests.get(url, headers=headers)
+        response_data = response.json()
+        currency_rate = {'currency': currency,
+                         'rate': response_data['rates']['RUB']}
+        result.append(currency_rate)
+
+    return result
